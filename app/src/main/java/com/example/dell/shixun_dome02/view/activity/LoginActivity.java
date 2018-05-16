35e161803d4efc7f0b5a4f0b99aae13ee9573f66
Your Name <13230024657@163.com>
2018-05-16 21:20:17 +0800

    aaa

---
+++ app/src/main/java/com/example/dell/shixun_dome02/view/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.dell.shixun_dome02.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dell.shixun_dome02.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
